6b9f11e5c3416f3c8db508527c9bd77ad11dddda
Jose Daniel <99700736+Jdalucard@users.noreply.github.com>
2022-06-07 16:16:10 -0500

    created calculetor.js

---
+++ index.js
@@ -0,0 +1,28 @@
+//1.Importaciones
+import {sum,multiplicar} from './lib/calculadora.mjs'
+
+//2.VARIABLES 
+const  getresult=document.querySelector('#get-result')
+const result=document.querySelector('#result')
+
+//3.Eventos
+getresult.addEventListener('click',(event)=>{
+    //detener la recarga por defecto
+    event.preventDefault()
+
+    //astraiga los datos de la variable
+    // a. Obtener dato de Formulario
+    const quantityOne=form["quantity-one"].value
+    //b. Convertir a entero
+    const parseQuatityOne=parseInt(quantityOne)
+    const quantitytwo=form["quantity-two"].value
+    const parseQuatitytwo=parseInt(quantitytwo)
+    const resultSum=sum(parseQuatityOne,parseQuatitytwo)
+
+    result.innerHTML=resultSum
+    
+    return
+    
+})
+
+
